def97d54fd25fc0e13a369d17da93bab0834d355
Keyann Williams <wkeyann@gmail.com>
2023-02-25 00:22:00 +0000

    Finished working on FAQ. Might add more questions.

---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,72 @@
+import { Box, useTheme, Typography } from "@mui/material";
+import Header from "../../components/Header";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { tokens } from "../../theme";
+
+const FAQ = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    return <Box m="20px">
+        <Header title="FAQ" subtitle="Frequently Asked Questions" />
+
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    Is this Application Useable?
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    No, it is simply a presentation of a react dashboard that could be
+                    used for any other website.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    Is all the code that allows this site to function following
+                    industry practices?
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Yes. All the code that runs this site is all up-to-date
+                    and industry standard as of Early March 2023.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    What dependancies does this application use?
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    This site uses React, Material UI, Nivo Charts, Formik, Fullcalendar,
+                     Data Grid and Yup.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    Why do you use an Image of Mark Zuckerburg for your example
+                    profile image?
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    I needed an image, and I thought it was funny.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+    </Box>
+}
+
+export default FAQ;
